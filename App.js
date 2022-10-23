3a75981803b7cfe1df9f1d38313986efc8530cfd
Hayotunday <idowudanielayotunde@gmail.com>
2022-10-23 16:26:24 +0100

    Updated app

--- App.js
+++ App.js
@@ -1,24 +1,33 @@
-import React, { useState } from 'react';
-import { StyleSheet, ScrollView, Text, View, KeyboardAvoidingView, TextInput, TouchableOpacity, Keyboard } from 'react-native';
-import Task from './components/Task';
+import React, { useState } from "react";
+import {
+	StyleSheet,
+	ScrollView,
+	Text,
+	View,
+	KeyboardAvoidingView,
+	TextInput,
+	TouchableOpacity,
+	Keyboard,
+} from "react-native";
+import Task from "./components/Task";
 
 export default function App() {
-  const [task, setTask] = useState();
-  const [taskItems, setTaskItems] = useState([]);
+	const [task, setTask] = useState();
+	const [taskItems, setTaskItems] = useState([]);
 
-  const handleAddTask = () => {
+	const handleAddTask = () => {
 		Keyboard.dismiss();
 		setTaskItems([...taskItems, task]);
 		setTask(null);
 	};
 
-  const completeTask = (index) => {
-    let itemsCopy = [...taskItems];
-    itemsCopy.splice(index, 1);
-    setTaskItems(itemsCopy);
-  }
+	const completeTask = (index) => {
+		let itemsCopy = [...taskItems];
+		itemsCopy.splice(index, 1);
+		setTaskItems(itemsCopy);
+	};
 
-  return (
+	return (
 		<View style={styles.container}>
 			{/* Today's Task */}
 			<ScrollView style={styles.tasksWrapper}>
@@ -62,50 +71,50 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#E8EAED',
-  },
-  tasksWrapper: {
-    paddingTop: 80,
-    paddingHorizontal: 20,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: 'bold',
-  },
-  items: {
-    marginTop: 30,
-    marginBottom: 10,
-  },
-  writeTaskWrapper: {
-    // position: 'absolute',
-    bottom: 20,
-    width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
-  },
-  input: {
-    paddingVertical: 15,
-    paddingHorizontal: 15,
-    backgroundColor: '#fff',
-    borderRadius: 20,
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-    width: 250,
-  },
-  addWrapper: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#fff',
-    borderRadius: 60,
-    borderColor: '#C0C0C0',
-    borderWidth: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  addText: {
-    
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#E8EAED",
+	},
+	tasksWrapper: {
+		paddingTop: 80,
+		paddingHorizontal: 20,
+	},
+	sectionTitle: {
+		fontSize: 24,
+		fontWeight: "bold",
+	},
+	items: {
+		marginTop: 30,
+		marginBottom: 10,
+	},
+	writeTaskWrapper: {
+		bottom: 20,
+		width: "100%",
+		flexDirection: "row",
+		justifyContent: "space-around",
+		alignItems: "center",
+	},
+	input: {
+		paddingVertical: 15,
+		paddingHorizontal: 15,
+		backgroundColor: "#fff",
+		borderRadius: 20,
+		borderColor: "#C0C0C0",
+		borderWidth: 1,
+		width: 250,
+	},
+	addWrapper: {
+		width: 60,
+		height: 60,
+		backgroundColor: "#fff",
+		borderRadius: 60,
+		borderColor: "#C0C0C0",
+		borderWidth: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+	addText: {
+		fontSize: 40,
+		fontWeight: "200",
+	},
 });
